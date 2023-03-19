a869ef61eb180b4e8cd03062d0792f25ca7725ba
Timothy Luna <timothy.d.luna@gmail.com>
2023-03-19 10:27:34 -0600

    fix(init): fix cli call

--- test/setup.sh
+++ test/setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Install terraform
+sudo snap install terraform --classic
 # Create a testing artifact folder.
 mkdir -p files/artifacts
 # Create an .ssh key for the testing routine.
@@ -15,7 +17,7 @@ sed "s/<USER_KEY>/$USER_KEY/g" files/user-data-template > files/artifacts/lunare
 lxc network create testing_net --type=bridge
 # Use a flavor of Ubuntu to create a VM using this network configuration and user configuration
 lxc launch ubuntu-daily:bionic test-container \
-    --config=cloud-init.network-config=$(cat files/network-config) \
+    --config=cloud-init.network-config="$(cat files/network-config)" \
     --config=cloud-init.user-data="$(cat files/artifacts/lunarengineer-bot-user-data)"
 # Attach that container to this testing network to ensure a unique IP.
 lxc network attach testing_net test-container eth0
--- test/stupid.sh
+++
@@ -1,3 +0,0 @@
-lxc launch ubuntu-daily:bionic test-container \
-    --config=cloud-init.network-config=$(cat files/network-config) \
-    --config=cloud-init.user-data="$(cat files/artifacts/lunarengineer-bot-user-data)"
