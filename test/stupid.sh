6c81e81327404db2308cb21025348f9ae2160a4f
Timothy Luna <timothy.d.luna@gmail.com>
2023-03-26 11:08:35 -0600

    chore

--- test/stupid.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -x
-USER_KEY="$(cat files/artifacts/lunarengineer-bot-key.pub)"
-sed "s/<USER_KEY>/\"$USER_KEY\"/g" files/user-data-template > files/artifacts/lunarengineer-bot-user-data
