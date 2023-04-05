f5ac1bb81ccde795c653050b65ba315d926e7c5e
Timothy Luna <timothy.d.luna@gmail.com>
2023-04-05 07:54:39 -0500

    chore(ntp): raspbian break on install

--- test/now.sh
+++ test/now.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-scp -i files/artifacts/lunarengineer-bot-key.pub lunarengineerbot@10.47.185.234:/etc/rancher/k3s/k3s.yaml ~/.kube/config
+scp -i files/artifacts/lunarengineer-bot-key lunarengineerbot@10.47.185.234:/etc/rancher/k3s/k3s.yaml ~/.kube/config
 # 3. Run teardown
